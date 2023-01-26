11137fffed6a7f2c2fc1bd08a685d991251ad9f6
EriSuzuki <sekka.alice@gmail.com>
2023-01-26 22:19:26 +0900

    [update] Testページ追加。

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('test.index')" :active="request()->routeIs('test.index')">
+                        {{ __('Test') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,12 +24,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
+// });
+
 // 複数のルートにmiddleware適用
 Route::middleware('auth')->group(function () {
     // Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// コントローラーごとにmiddleware適用
+Route::controller(TestController::class)->middleware(['auth'])->group(function(){
+    Route::get('/test', 'index')->name('test.index');
 });
 
 require __DIR__.'/auth.php';
