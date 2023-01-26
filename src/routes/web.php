b472de38bc11232747436762ea9bc7b46967683b
EriSuzuki <sekka.alice@gmail.com>
2023-01-26 20:53:38 +0900

    [wip] ルーティングにメモを記載。

--- src/routes/web.php
+++ src/routes/web.php
@@ -18,11 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// 1つのルートにmiddleware適用
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// 複数のルートにmiddleware適用
 Route::middleware('auth')->group(function () {
+    // Route::get('/dashboard', function () { return view('dashboard'); })->name('dashboard');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
